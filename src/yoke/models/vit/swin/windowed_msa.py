01a1f3bdbb28dad4747ff538e018a1beb25a2c08
dschodt-lanl <dschodt@lanl.gov>
2025-03-12 14:59:46 -0600

    Fix some assertion messages in windowed_msa

--- src/yoke/models/vit/swin/windowed_msa.py
+++ src/yoke/models/vit/swin/windowed_msa.py
@@ -72,9 +72,9 @@ class WindowMSA(nn.Module):
         except AssertionError as e:
             msg_tuple = (
                 "Patch-grid 2:",
-                patch_grid_size[0],
+                patch_grid_size[1],
                 "Window-size 2:",
-                window_size[0],
+                window_size[1],
             )
             e.args += msg_tuple
             raise
@@ -225,9 +225,9 @@ class ShiftedWindowMSA(nn.Module):
         except AssertionError as e:
             msg_tuple = (
                 "Patch-grid 2:",
-                patch_grid_size[0],
+                patch_grid_size[1],
                 "Window-size 2:",
-                window_size[0],
+                window_size[1],
             )
             e.args += msg_tuple
             raise
@@ -444,9 +444,9 @@ class WindowCosMSA(nn.Module):
         except AssertionError as e:
             msg_tuple = (
                 "Patch-grid 2:",
-                patch_grid_size[0],
+                patch_grid_size[1],
                 "Window-size 2:",
-                window_size[0],
+                window_size[1],
             )
             e.args += msg_tuple
             raise
@@ -604,9 +604,9 @@ class ShiftedWindowCosMSA(nn.Module):
         except AssertionError as e:
             msg_tuple = (
                 "Patch-grid 2:",
-                patch_grid_size[0],
+                patch_grid_size[1],
                 "Window-size 2:",
-                window_size[0],
+                window_size[1],
             )
             e.args += msg_tuple
             raise
